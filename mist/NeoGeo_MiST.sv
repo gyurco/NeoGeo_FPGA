3a198e6c49d2d0834493c7a5f5e7e95beed1cd20
Gyorgy Szombathelyi <8644936+gyurco@users.noreply.github.com>
2024-02-07 23:01:40 +0100

    SiDi128: upgrade to final pinout

--- mist/NeoGeo_MiST.sv
+++ mist/NeoGeo_MiST.sv
@@ -77,6 +77,12 @@ module NeoGeo_MiST(
 	output        I2S_LRCK,
 	output        I2S_DATA,
 `endif
+`ifdef I2S_AUDIO_HDMI
+	output        HDMI_MCLK,
+	output        HDMI_BCK,
+	output        HDMI_LRCK,
+	output        HDMI_SDATA,
+`endif
 `ifdef SPDIF_AUDIO
 	output        SPDIF,
 `endif
@@ -1257,6 +1263,14 @@ i2s i2s (
 	.left_chan(au_left[16:1]),
 	.right_chan(au_right[16:1])
 );
+`ifdef I2S_AUDIO_HDMI
+assign HDMI_MCLK = 0;
+always @(posedge CLK_48M) begin
+	HDMI_BCK <= I2S_BCK;
+	HDMI_LRCK <= I2S_LRCK;
+	HDMI_SDATA <= I2S_DATA;
+end
+`endif
 `endif
 
 `ifdef SPDIF_AUDIO
